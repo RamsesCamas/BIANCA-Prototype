025c41ee1404eccf904f6f583d1e127eab0bd263
RamsesCamas <machiniram@gmail.com>
2021-11-18 02:24:47 -0600

    Prueba 1 fallida

--- pruebas_spe_reg/main.py
+++ pruebas_spe_reg/main.py
@@ -1,5 +1,17 @@
-
-
-
+import speech_recognition as speech
+#Prueba fallida
 if __name__ == '__main__':
-    pass
+    recog = speech.Recognizer()
+    print('Hola')
+    mic = speech.Microphone()
+    with mic as audio_file:
+        print('Speak please')
+        recog.adjust_for_ambient_noise(audio_file)
+        audio = recog.listen(audio_file)
+        print('Convirtiendo a texto')
+        print('Dijiste:')
+        try:
+            print(recog.recognize_bing(audio))
+        except Exception as e:
+            print('Error: ',e)
+    
